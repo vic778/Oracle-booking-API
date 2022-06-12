012ed50d56db46d820c2ea142c194704f0fbb099
Victoir Barh <77842237+vic778@users.noreply.github.com>
2022-06-12 20:36:56 +0300

    auth (#9)
    * Generate a token key
    
    * Add routes for a user
    
    * Add the validations
    
    * Add cros
    
    * Fix linter errors

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :authorized, only: %i[show update destroy]
+  before_action :set_user, only: %i[show update destroy]
+
+  def index
+    @users = User.all
+    render json: @users if @users
+  end
+
+  def show
+    render json: @user if @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      token = encode_token({ user_id: @user.id })
+      render json: { success: true, messsage: "User created successfully" }, status: :created
+    else
+      render json: { success: false, message: @user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: { success: true, messsage: "User updated successfully" }, status: :created
+    else
+      render json: { success: false, message: @user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: { success: true, messsage: "User deleted successfully" }, status: :created
+    else
+      render json: { success: false, message: @user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+end
