d93ec8d6071c2c9472155dda6db58052a19d5e7d
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-13 20:34:16 +0300

    Make if a car is not favorites can be deleted

--- app/controllers/api/v2/cars_controller.rb
+++ app/controllers/api/v2/cars_controller.rb
@@ -1,20 +1,24 @@
 class Api::V2::CarsController < ApplicationController
   before_action :authorized
-  before_action :set_car, only: %i[update destroy]
+  before_action :set_car, only: %i[update]
 
   def index
     @cars = Car.all
-    render json: @cars
+    if @cars
+    render json: @cars 
+    else
+      render json: { success: false, message: "[]" }, status: :not_found
+    end
   end
 
   def show
     @car = Car.find(params[:id])
-    render json: @car
+    render json: @car 
   end
 
   def create
     @car = Car.create(car_params.merge(user: @user))
-    if @car.valid?
+    if @car.save
       render json: { success: true, message: "Car created successfully", car: @car }, status: :created
     else
       render json: { success: false, message: "Car not created", errors: @car.errors.full_messages },
@@ -32,8 +36,16 @@ class Api::V2::CarsController < ApplicationController
   end
 
   def destroy
-    @car.destroy
-    render json: { success: true, message: "Car deleted successfully" }, status: :ok
+   if @car = @user.cars.find_by(id: params[:id])
+      if @car.favorite == false
+        @car.destroy
+        render json: { success: true, message: "Car deleted successfully" }, status: :ok
+      else
+        render json: { success: false, message: "This car is favorited by someone else" }, status: :unprocessable_entity
+      end
+    else
+      render json: { success: false, message: "Only the owner can delete this car" }, status: :not_found
+    end
   end
 
   private
