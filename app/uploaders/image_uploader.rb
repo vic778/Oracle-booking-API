9fb4983a296c7dd054be42c9d83f4cc98f7bc8fb
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-25 20:37:17 +0300

    Add cloudinary

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,29 +1,29 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  if Rails.env == 'test' || 'development'
-    storage :file
-    def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
-  else
+  # if Rails.env == 'test' || 'development'
+  #   storage :file
+  #   def store_dir
+  #     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #   end
+  # else
     include Cloudinary::CarrierWave
 
-    process convert: 'png'
-    process tags: ['profile_picture']
+  #   process convert: 'png'
+  #   process tags: ['profile_picture']
 
-    # version :standard do
-    #   process resize_to_fill: [100, 150, :north]
-    # end
+  #   # version :standard do
+  #   #   process resize_to_fill: [100, 150, :north]
+  #   # end
 
-    # version :thumbnail do
-    #   resize_to_fit(50, 50)
-    # end
+  #   # version :thumbnail do
+  #   #   resize_to_fit(50, 50)
+  #   # end
 
-    CarrierWave.configure do |config|
-      config.cache_storage = :file
-    end
-  end
+  #   CarrierWave.configure do |config|
+  #     config.cache_storage = :file
+  #   end
+  # end
 
-  def extension_allowlist
-    %w[jpg jpeg gif png]
-  end
+  # def extension_allowlist
+  #   %w[jpg jpeg gif png]
+  # end
 end
